3453573f51860c8f28c7059c755b558169b17121
OCC-0111 <occ-0111@occ-0111s-mbp.home>
2023-09-01 15:38:51 +0200

    homework done

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
 var id = 1;
-const welcomeMessage = {
+let welcomeMessage = {
   id: id,
   from: "Ali",
   text: "Welcome to MigraCode chat system!",
@@ -24,6 +25,47 @@ app.get("/", function (request, response) {
 
 //START OF YOUR CODE...
 
+//read all messages
+app.get("/messages", (req,res) => {
+  res.send(messages);
+});
+
+//create a new message !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+app.post("/messages", (req, res) => {
+  const newMessage = req.body;
+  newMessage.id = messages.length + 1;
+  messages.push(newMessage);
+  res.status(201).send({
+    message: "New message created successfully",
+    data: newMessage,
+  });
+});
+
+//Read one message specified by an ID
+app.get("/messages/:id", (req, res) => {
+  const requestedId = req.params.id;
+  const filteredById = messages.find((element) => element.id == requestedId);
+  if (!filteredById){
+    return res
+    .status(404)
+    .send(`The message with id number ${requestedId} was not found!`)
+  };
+  res.send(filteredById);
+});
+
+//Delete a message by ID
+app.delete("/messages/:id", (req, res) => {
+  const requestedId = req.params.id;
+  const filteredById = messages.find((element) => element.id == requestedId);
+  if (!filteredById){
+    return res
+    .status(404)
+    .send(`The message with id number ${requestedId} was not found!`)
+  };
+  const findElementIndex = messages.findIndex((element) => element.id == requestedId);
+  messages.splice(findElementIndex, 1);
+  res.status(200).send(`The message with id number ${requestedId} was succesfully deleted.`);
+});
 
 //...END OF YOUR CODE
 
